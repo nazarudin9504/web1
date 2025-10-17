a7bc4ac2044fa41b15f2a736615dc9cca9bfff39
Nazarudin <160707783+nazarudin9504@users.noreply.github.com>
2025-10-17 21:37:59 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Web1 - Aplikasi 1</h1>";
+echo "<p>Diakses dari github1</p>";
+echo "<p>Server IP: " . $_SERVER['SERVER_ADDR'] . " | Port: 2000</p>";
+echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
+?>
